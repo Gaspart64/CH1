dc0804b82579ba4366a6483c5b8b7a72884aba97
Gaspart64 <147103544+Gaspart64@users.noreply.github.com>
2024-11-28 15:34:09 -0600

    Update chess-pgn-trainer.js

--- assets/chess-pgn-trainer.js
+++ assets/chess-pgn-trainer.js
@@ -64,7 +64,7 @@ let PauseendDateTime;
 let startDateTime = new Date();
 let pauseDateTimeTotal = 0;
 
-const { enableScroll, disableScroll } = require("./toggle-scrollbar.js");
+const { enableScroll, disableScroll } = require("./assets/toggle-scrollbar.js");
 
 // -------------
 // Initial Setup
